00dc7b6af28c7a6e93bc9950caacdb35a33e809a
Necmi Gunduz <necmigunduz@gmail.com>
2024-08-06 13:17:16 +0300

    complete: userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,14 +8,29 @@ const register = async (req, res) => {
         const user = new User(userInfo);
         await user.save();
         const token = jwt.sign({ _id: user._id }, secretJWT);
-        res.status(201).send({ user, token });   
+        res.status(201).send({ user, token });
     } catch (error) {
         res.status(400).send(`error: ${error}`);
     };
 };
 
-const login = async (req, res) => {};
+const login = async (req, res) => {
+    try {
+        const user = User.findOne({ email: req.body.email });
+        if (!user || (await user.comparePassword(req.body.password))) {
+            res.status(401).send('Invalid login credentials!')
+            throw new Error('Invalid login credentials!')
+        };
 
-const getProfile = async (req, res) => {};
+        const token = jwt.sign({ _id: user._id }, secretJWT);
+        res.status(200).send({ user, token });
+    } catch (error) {
+        res.status(400).send({ error: 'Invalid login credentials' });
+    }
+};
+
+const getProfile = async (req, res) => {
+    res.send(req.user);
+};
 
 module.exports = { register, login, getProfile };
