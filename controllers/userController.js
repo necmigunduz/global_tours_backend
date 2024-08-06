48b0ed502f5afeeeb0d269ec16d64b3bbaf83eed
Necmi Gunduz <necmigunduz@gmail.com>
2024-08-06 13:03:51 +0300

    add: register method to user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,18 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const serializeUser = require('../serializers/UserSerializer');
-
-const register = async (req, res) => {};
+const secretJWT = process.env.JWT_SECRET;
+const register = async (req, res) => {
+    try {
+        const userInfo = await serializeUser(req.body);
+        const user = new User(userInfo);
+        await user.save();
+        const token = jwt.sign({ _id: user._id }, secretJWT);
+        res.status(201).send({ user, token });   
+    } catch (error) {
+        res.status(400).send(`error: ${error}`);
+    };
+};
 
 const login = async (req, res) => {};
 
